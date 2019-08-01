4c2dbe41fce8c7d3a989748e6a240ec0ef62d8f4
mba-open <mohamed.bahmed@open-groupe.com>
2019-08-01 16:53:50 +0200

    first commit in symfony

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
+
+/**
+ * Class ProductController
+ * @Rest\Route("/products")
+ */
+class ProductController extends AbstractFOSRestController
+{
+    /**
+     * List the results of a product.
+     * @Rest\Get("/list")
+     *
+     * @SWG\Response(response=Response::HTTP_OK, description="Ok")
+     * @SWG\Tag(name="test")
+     *
+     *
+     */
+    public function getAction()
+    {
+        try {
+           // echo 'toto'; exit;
+            $em = $this->getDoctrine()->getManager();
+            $products = $em->getRepository(Product::class)->findAll();
+            return $this->view($products, Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return $this->view($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+}
